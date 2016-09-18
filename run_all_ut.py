d0ba07f00eaef9c3b1609e63174f1bc3a952d64c
Jason Atkins <jatkins1995@gmail.com>
2016-09-17 20:25:11 -0500

    Removing outdated comment

--- run_all_ut.py
+++ run_all_ut.py
@@ -7,7 +7,6 @@ import unittest
 test_running_file = open ("temp_test_running_file.py", "w")
 test_running_file.write("#!/usr/bin/python\n")
 test_files = subprocess.check_output('grep -rl "TestCase" . --exclude=run_all_ut.py --exclude=*.pyc', shell=True).split('\n')[:-1]
-#To lazy to deal with the extre empty sting at the array so subtracting 1 off the length
 test_file_count = len(test_files)
 for file_path in test_files:
 	include_path = file_path[2:-3].replace('/', '.')
